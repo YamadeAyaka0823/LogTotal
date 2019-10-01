151d6a400d1f042496439b116432fe6000c4f047
yamadeayaka <ayaka.yamade@rakus-partners.co.jp>
2019-10-01 13:41:48 +0900

    firstcommit

---
+++ src/main/java/com/example/LogTotalApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogTotalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogTotalApplication.class, args);
+	}
+
+}
